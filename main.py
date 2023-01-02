fa442461e1ee194ac69cc46c2b552e07ed1fe820
Kaila <120765858+kaila200@users.noreply.github.com>
2023-01-02 09:30:47 +0000

    i changed the main

---
+++ main.py
@@ -0,0 +1,73 @@
+print("Hello, what's your name?")
+user_name = input()
+print("Thats a pretty name, " +  user_name)
+
+random_number = input("What is you favorite number 1-10?")
+
+if random_number == '5':
+   print("Omg we have the same favorite number!")
+else:
+  print("That's a good number!")
+
+major_class = input("Whats your favorite major class in school?")
+if major_class == 'science':
+  print("No way we have the same favorite major class!")
+else:
+  print("That is a really great major class.")
+
+favorite_holiday = input("What is your favorite holiday?")
+if favorite_holiday == 'christmas':
+  print("Christmas is my favorite holiday toooo")
+else:
+  print("Wow that a good holiday as well.") 
+
+your_birthday = input("Is it your birthday?")
+if your_birthday == 'yes':
+  print("Happy Birthday to you!") 
+  print("Happy Birthday to you!")
+  print("Happy Birthday to you and many more!")
+  
+else:
+  input("when is it?")
+  print ("Ohhh thats a great month to have a birthday in!")
+
+print("That all the questions I have.")
+start_over = input("Do you wish to talk again yes or no?") 
+if start_over == 'no':
+  print("Good Byeee")
+
+if start_over == 'yes':
+  print("Hello, what's your name?")
+user_name = input()
+print("Thats a pretty name, " +  user_name)
+
+random_number = input("What is you favorite number 1-10?")
+
+if random_number == '5':
+   print("Omg we have the same favorite number!")
+else:
+  print("That's a good number!")
+
+major_class = input("Whats your favorite major class in school?")
+if major_class == 'science':
+  print("No way we have the same favorite major class!")
+else:
+  print("That is a really great major class.")
+
+favorite_holiday = input("What is your favorite holiday?")
+if favorite_holiday == 'christmas':
+  print("Christmas is my favorite holiday toooo")
+else:
+  print("Wow that a good holiday as well.") 
+
+your_birthday = input("Is it your birthday?")
+if your_birthday == 'yes':
+  print("Happy Birthday to you!") 
+  print("Happy Birthday to you!")
+  print("Happy Birthday to you and many more!")
+  
+else:
+  input("when is it?")
+  print ("Ohhh thats a great month to have a birthday in!")
+
+print("That all the questions I have.")
